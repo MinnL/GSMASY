a83d58e1430879043137c0310e5ec8656b2e8da7
Luke Yeh <luke.yeh@nyu.edu>
2019-08-28 07:41:22 -0400

    add download file and session

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+from flask import session
 import scholarly
+from sklearn.feature_extraction import text
+from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
+import numpy as np
+from stemming.porter2 import stem
 
 app = Flask(__name__)
+app.secret_key = "super secret key"
 
 
 
@@ -41,6 +48,8 @@ def select():
         if index == limit:
             break
     listc = [[x, y, z] for x, y, z in zip(i, t, a)]
+    session['abstract'] = a
+    session['title'] = t
     return render_template('selectpaper.html', titles=t, abstracts=a, lc=listc)
 
 
@@ -48,19 +57,66 @@ def select():
 def result():
     sid = request.form['sid']
     sid = sid.split()
+    sid = [int(i) for i in sid]
+    a = session.get('abstract')
+    t = session.get('title')
 
-    # # get selected abstract
-    # sa = []
-    # for index, item in enumerate(sid):
-    #     sa.append(a[item - 1])
-    # # print(sa)
+    # get selected abstract
+    sa = []
+    for index, item in enumerate(sid):
+        sa.append(a[item - 1])
+
+    # get abstract excluded from selected item
+    sid.sort(reverse=True)
+    for index, item in enumerate(sid):
+        a.pop(item - 1)
+        t.pop(item - 1)
     #
-    # # get abstract excluded from selected item
-    # x.sort(reverse=True)
-    # for index, item in enumerate(x):
-    #     a.pop(item - 1)
-    #     t.pop(item - 1)
-    return render_template('result.html', sid=sid)
+    # make non-selected articles a data frame for calculation
+    ta_df = pd.DataFrame({'Title': t, 'Abstract': a})
+
+    # combine 2 lists to one
+    s = " "
+    sa = s.join(sa)  # make all selected abstracts into one
+    ca = a
+    ca.append(sa)  # append the selected abstracts
+    #
+    # Stopwords
+    my_additional_stop_words = ["author", "and", "of", "the", "research", "\n"]
+    stop_words = text.ENGLISH_STOP_WORDS.union(my_additional_stop_words)
+    #
+    # STEM
+    ca = [[stem(word) for word in sentence.split(" ")] for sentence in ca]
+    y = len(ca)
+    for i in range(0, y):
+        ca[i] = " ".join(ca[i])
+
+    # build tf-idf matrix
+    tfidf = text.TfidfVectorizer(input=ca, stop_words=stop_words, analyzer='word', lowercase=True)
+    matrix = tfidf.fit_transform(ca)
+
+    # calculate similarity score
+    sim_unigram = cosine_similarity(matrix)
+
+    # build function to get similar score for all articles by locating them
+    def get_similar_papers(x):
+        return ta_df.loc[np.argsort(-x)]
+
+    recomPaper = get_similar_papers(sim_unigram[-1])
+
+    sim_unigram[-1][::-1].sort()  # descending
+    # # sim_unigram[-1].sort() #ascending
+
+    recomPaper["Similar_score"] = sim_unigram[-1]
+
+    # get the 50 papers with highest similarity
+    recomPaper50 = recomPaper[:51]
+    recomPaper50 = recomPaper50.dropna()
+    #
+    # output file
+    recomPaper50.to_csv("recom50papers.csv")
+
+    return render_template('result.html', sid=sid, a=a, t=t)
 
 
 
